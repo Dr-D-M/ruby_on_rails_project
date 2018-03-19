17298d016468dfe1e6c947c3521cd6efe7be83af
Dr-D-M <david_garcia.p@hotmail.com>
2018-03-19 11:46:02 -0600

    Sorcery authentication complete

--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -2,22 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 root to: 'articles#index'
-resources :articles
+  resources :articles do
+  	resources :comments
+  	resources :reviews
+  end
+  resources :tags
+  resources :authors
+  resources :author_sessions, only: [ :new, :create, :destroy ]
 
-
-resources :article do
-  resources :comments 
-end
-Blogger::Application.routes.draw do
-
-   root to: 'articles#index'
-   resources :articles do
-     resources :comments
- end
-   resources :tags
-
-end
-
-
+get 'login'  => 'author_sessions#new'
+get 'logout' => 'author_sessions#destroy'
 end
 
