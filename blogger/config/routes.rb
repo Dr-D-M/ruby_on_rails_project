c03021b58e04d53e83a2d36954e1b511e347723e
Dr-D-M <david_garcia.p@hotmail.com>
2018-03-19 22:34:17 -0600

    heroku initial commit

--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-root 'drinks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root 'drinks#index'
 
 root to: 'articles#index'
   resources :articles do
